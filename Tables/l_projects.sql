6bf189db2ddcfcd27090e116575aec6a6f3047d4
Aleksandr Klein <mr.kleyn@mail.ru>
2018-05-29 15:11:41 +0500

    Update l_projects.sql

--- Tables/l_projects.sql
+++ Tables/l_projects.sql
@@ -2,7 +2,7 @@ declare
     object_not_found exception;
     pragma exception_init(object_not_found, -00942);
 begin
-    execute immediate ('drop table l_plan.l_variable');
+    execute immediate ('drop table l_plan.l_projects');
 exception
     when object_not_found then
         null;
@@ -10,20 +10,26 @@ exception
         dbms_output.put_line('Ошибка '||sqlerrm);
 end;
 /
-create table l_plan.l_variable (id          number        not null,
-                                param_name  varchar2(255) not null,
-                                param_desc  varchar2(1024))
+create table l_plan.l_projects (id    number       not null,
+                                ccode varchar2(50),
+                                name  varchar2(256))
 tablespace l_plan;
-
-comment on table l_plan.l_variable is 'Справочник Переменных';
-
-comment on column l_plan.l_variable.id         is 'ID переменной';
-comment on column l_plan.l_variable.param_name is 'Переменная как в параметрах';
-comment on column l_plan.l_variable.param_desc is 'Описание переменной';
-
-create or replace trigger l_plan.tr_l_variable 
-before insert
-on l_plan.l_variable 
+/
+comment on table l_plan.l_projects is 'Справочник проектов';
+/
+comment on column l_plan.l_projects.id    is 'ID проекта';
+comment on column l_plan.l_projects.ccode is 'Символьный код проекта';
+comment on column l_plan.l_projects.name  is 'Наименование проекта';
+/
+create unique index l_plan.projects_pk_id on l_plan.l_projects
+(id)
+tablespace l_plan;
+/
+alter table l_plan.l_projects
+add (constraint projects_pk_id primary key (id) using index projects_pk_id);
+/
+create or replace trigger l_plan.tr_l_projects before insert
+on l_plan.l_projects 
 referencing new as new old as old
 for each row
 begin
@@ -32,9 +38,7 @@ begin
 
         select (max(id)+1) 
         into   :new.id 
-        from   l_plan.l_variable;
-    
+        from   l_plan.l_projects;
     end if;
-
 end;
 /
